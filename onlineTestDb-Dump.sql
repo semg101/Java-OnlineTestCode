01c9bd0d8301e17df77e85128456f3923ea3085a
semg101 <gansosaker@yahoo.fr>
2020-05-14 12:46:05 +0100

    Java Online Test Code

---
+++ onlineTestDb-Dump.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Community Edition- MySQL GUI v5.22a
+Host - 5.0.19-nt : Database - online_test
+*********************************************************************
+Server version : 5.0.19-nt
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+create database if not exists `online_test`;
+
+USE `online_test`;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `answer` */
+
+DROP TABLE IF EXISTS `answer`;
+
+CREATE TABLE `answer` (
+  `id` int(11) NOT NULL auto_increment,
+  `question_id` int(11) NOT NULL,
+  `answer` varchar(50) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `answer` */
+
+insert  into `answer`(`id`,`question_id`,`answer`) values (1,1,'Object Oriented'),(2,2,'Before the class');
+
+/*Table structure for table `question` */
+
+DROP TABLE IF EXISTS `question`;
+
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL auto_increment,
+  `question` varchar(200) NOT NULL,
+  `opt1` varchar(50) NOT NULL,
+  `opt2` varchar(50) NOT NULL,
+  `opt3` varchar(50) NOT NULL,
+  `opt4` varchar(50) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `question` */
+
+insert  into `question`(`id`,`question`,`opt1`,`opt2`,`opt3`,`opt4`) values (1,'Java is ?','Object Oriented','Procedural','Asembly','Machin Level'),(2,'Static block loaded','Afer the class','Before the class','never','No such variable in java');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
